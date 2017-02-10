9df062799a230e3d423cbb437a751d86d6858dd0
Erik Rose <erik@mozilla.com>
2017-02-10 17:06:36 -0500

    Pin down dependencies.
    Don't stray into new major versions which may be backward-incompatible. Do mention Jinja as a top-level dependency, even though Sphinx brings it in at the moment, because we import directly from it.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     test_suite='nose.collector',
     url='https://github.com/erikrose/sphinx-js',
     include_package_data=True,
-    install_requires=['docutils', 'six', 'Sphinx'],
+    install_requires=['docutils', 'Jinja2>2.0<3.0', 'six<2.0', 'Sphinx<2.0'],
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
