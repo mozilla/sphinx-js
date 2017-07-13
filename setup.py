d097913a62dec917c98e28f513e454162e28b571
Erik Rose <erik@mozilla.com>
2017-07-13 17:32:28 -0400

    Bump version to 2.0.1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description=open('README.rst', 'r', encoding='utf8').read()
 
 setup(
     name='sphinx-js',
-    version='2.0',
+    version='2.0.1',
     description='Support for using Sphinx on JSDoc-documented JS code',
     long_description=long_description,
     author='Erik Rose',
