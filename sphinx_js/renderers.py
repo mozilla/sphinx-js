9e4abb781ba2f9650eeacb026abebd3a7ea6f264
Wim Yedema <wim.yedema@gmail.com>
2018-03-15 13:47:23 +0100

    Separate type from :param:
    Having the type inside the :param: attribute does not work when it
    contains spaces. There is a separate :type: attribute that can be used
    for more complex types. This change generates the :type: attribute.

--- sphinx_js/renderers.py
+++ sphinx_js/renderers.py
@@ -117,11 +117,13 @@ class JsRenderer(object):
         tail comes after.
 
         """
-        FIELD_TYPES = OrderedDict([('params', _params_formatter),
+        FIELD_TYPES = [('params', _params_formatter),
+                       ('params', _param_type_formatter),
                                    ('properties', _params_formatter),
+                       ('properties', _param_type_formatter),
                                    ('exceptions', _exceptions_formatter),
-                                   ('returns', _returns_formatter)])
-        for field_name, callback in iteritems(FIELD_TYPES):
+                       ('returns', _returns_formatter)]
+        for field_name, callback in FIELD_TYPES:
             for field in doclet.get(field_name, []):
                 description = field.get('description', '')
                 unwrapped = sub(r'[ \t]*[\r\n]+[ \t]*', ' ', description)
@@ -240,14 +242,15 @@ def _returns_formatter(field, description):
 
 def _params_formatter(field, description):
     """Derive heads and tail from ``@param`` blocks."""
-    heads = ['param']
-    types = _or_types(field)
-    if types:
-        heads.append(types)
-    heads.append(field['name'])
+    heads = ['param', field['name']]
     tail = description
     return heads, tail
 
+def _param_type_formatter(field, description):
+    """Derive heads and tail from ``@param`` blocks."""
+    heads = ['type', field['name']]
+    tail = _or_types(field)
+    return heads, tail
 
 def _exceptions_formatter(field, description):
     """Derive heads and tail from ``@throws`` blocks."""
