bbd37f93fe679f9df8b47fe04384ee68d943a89f
Erik Rose <erik@mozilla.com>
2018-03-21 12:24:34 -0400

    Remove unneeded bits from the recommonmark tests' Sphinx config.

--- tests/test_common_mark/source/docs/conf.py
+++ tests/test_common_mark/source/docs/conf.py
@@ -1,50 +1,19 @@
-#!/usr/bin/env python3
-
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
-author = 'Jam Risser'
-
-copyright = '2018, Jam Risser'
-
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 extensions = [
     'sphinx.ext.mathjax',
     'sphinx_js'
 ]
 
-html_static_path = ['_static']
-
-html_theme = 'sphinx_rtd_theme'
-
-htmlhelp_basename = 'sphinx_hello_worlddoc'
-
-language = None
-
+source_suffix = ['.rst', '.md']
 master_doc = 'index'
-
-needs_sphinx = '1.0'
-
-#primary_domain = 'js'
-
-project = 'sphinx-hello-world'
-
-pygments_style = 'sphinx'
-
-release = '0.0.1'
-
+author = 'Jam Risser'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 source_parsers = {
     '.md': CommonMarkParser
 }
-
-source_suffix = ['.rst', '.md']
-
-templates_path = ['_templates']
-
-todo_include_todos = False
-
-version = '0.0.1'
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
