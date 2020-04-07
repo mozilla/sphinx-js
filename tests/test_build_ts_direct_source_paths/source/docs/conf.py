10738aa14ddd65f7dde7363ffc0fa3ab1fb2f78f
Reuben Morais <reuben.morais@gmail.com>
2020-04-07 13:39:31 +0200

    Add a test with js_source_path pointing directly to TypeScript files

---
+++ tests/test_build_ts_direct_source_paths/source/docs/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+extensions = [
+    'sphinx_js'
+]
+
+# Minimal stuff needed for Sphinx to work:
+source_suffix = '.rst'
+master_doc = 'index'
+author = u'Erik Rose'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+jsdoc_config_path = '../tsconfig.json'
+js_source_path = '../class.ts'
+js_language = 'typescript'
