29160f6511c76be9973030eea4a783d94c115d34
Erik Rose <erik@mozilla.com>
2020-07-14 19:33:20 -0400

    Remove dead `_name` method.

--- sphinx_js/renderers.py
+++ sphinx_js/renderers.py
@@ -101,10 +101,6 @@ class JsRenderer(object):
         template = env.get_template(self._template)
         return template.render(**self._template_vars(dotted_name, full_path, doclet))
 
-    def _name(self):
-        """Return the JS function or class longname."""
-        return self._arguments[0].split('(')[0]
-
     def _formal_params(self, doclet):
         """Return the JS function or class params, looking first to any
         explicit params written into the directive and falling back to those in
