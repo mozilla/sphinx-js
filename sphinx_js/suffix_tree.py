e16789fbe5a9edc6eafb96fafdf2929d2603825b
Erik Rose <erik@mozilla.com>
2020-09-03 19:02:44 -0400

    Rewrite large parts of the suffix tree.
    Fix several crashers. Most of the problems stemmed from not realizing (and correctly dealing with or reporting) the possibility of a node holding both a value and a further subtree. The new implementation is based on a sounder understanding of the state space and is better tested and explained.
    
    I'm hoping this also fixes https://github.com/mozilla/sphinx-js/pull/169#issuecomment-779092066.

--- sphinx_js/suffix_tree.py
+++ sphinx_js/suffix_tree.py
@@ -1,33 +1,34 @@
-from collections import namedtuple
-
-
-Value = namedtuple('Value', ['value'])
-
-
 class SuffixTree(object):
-    """A suffix tree where the nodes are segments of a JS object path, like
-    "dir/" or "someObject#" or "max"
+    """A suffix tree in which you can use anything hashable as a path segment
+    and anything at all as a value
 
     """
     def __init__(self):
+        #: Internal structure is like... ::
+        #:
+        #:     Tree = {value?: Any,
+        #:             subtree?: {segmentFoo: Tree, segmentBar: Tree, ...}}
+        #
+        #: A Tree can have a value key, a subtree key, or both. Subtree dicts
+        #: always have at least 1 key. Every subtree has at least one value,
+        #: directly or indirectly. ``self._tree`` itself is a Tree.
         self._tree = {}
 
     def add(self, unambiguous_segments, value):
         """Add an item to the tree.
 
         :arg unambiguous_segments: A list of path segments that correspond
-            uniquely to the object
-        :arg value: Any value representing the JS object
+            uniquely to the stored value
+        :arg value: Any value you want to fetch by path
 
         """
         tree = self._tree
-        for seg in reversed(unambiguous_segments[1:]):
-            tree = tree.setdefault(seg, {})
-        seg = unambiguous_segments[0]
-        if seg in tree:
+        for seg in reversed(unambiguous_segments):
+            tree = tree.setdefault('subtree', {}).setdefault(seg, {})
+        if 'value' in tree:
             raise PathTaken(unambiguous_segments)
         else:
-            tree[seg] = Value(value)
+            tree['value'] = value
 
     def add_many(self, segments_and_values):
         """Add a batch of items to the tree all at once, and collect any
@@ -54,31 +55,47 @@ class SuffixTree(object):
 
         :arg segments list: A possibly ambiguous suffix of a path
 
-        If zero or multiple paths are found with the given suffix, raise an
-        error.
+        If no paths are found with the given suffix, raise SuffixNotFound. If
+        multiple are found, raise SuffixAmbiguous. This is true even if
+        the given suffix is a full path to a value: we want to flag the
+        ambiguity to the user, even if it might sometimes be more convenient to
+        assume the intention was to present a full path.
 
         """
-        full_path = []
+        # Keep walking down subtrees (returning NotFound if failed) until we
+        # run out of segs:
         tree = self._tree
         for seg in reversed(segments):
             try:
-                tree = tree[seg]
-                full_path.append(seg)
+                tree = tree['subtree'][seg]
             except KeyError:
                 raise SuffixNotFound(segments)
 
-        # Follow all the 1-key dicts. These are the paths that are unambiguous.
-        while len(tree) == 1:
-            if isinstance(tree, Value):
-                full_path.reverse()
-                return tree.value, full_path
-            # It's a dict. Traverse the only key:
-            key = next(iter(tree.keys()))
-            tree = tree[key]
-            full_path.append(key)
+        # If there's only a value there, return it:
+        if 'value' in tree:
+            if 'subtree' in tree:
+                raise SuffixAmbiguous(segments, list(tree['subtree'].keys()), or_ends_here=True)
+            return tree['value'], segments
 
-        # We hit a >1-key dict, an ambiguous path:
-        raise SuffixAmbiguous(segments, list(tree.keys()))
+        # Else if there's a subtree, follow its 1-key subtrees forever, since
+        # there's no ambiguity there:
+        additional_segments = []
+        while len(tree.get('subtree', {})) == 1:
+            only_key = next(iter(tree['subtree'].keys()))
+            tree = tree['subtree'][only_key]
+            additional_segments.append(only_key)
+
+        # If we arrived at a spot with multiple possiblities, yell:
+        if len(tree.get('subtree', {})) > 1:
+            raise SuffixAmbiguous(segments, list(tree['subtree'].keys()))
+
+        # Otherwise, return the found value. There must always be a value here
+        # because add() always eventually adds a value after a chain of
+        # subtrees. If there were multiple subrees from here, we would have
+        # raised SuffixAmbiguous above. If there were a single one, we would
+        # have followed it. So, since subtrees always eventually terminate in a
+        # value, we must be at one now.
+        return tree['value'], (list(reversed(additional_segments)) + segments)
 
     def get(self, segments):
         return self.get_with_path(segments)[0]
@@ -124,9 +141,11 @@ class SuffixNotFound(SuffixError):
 class SuffixAmbiguous(SuffixError):
     """There were multiple keys found ending in the suffix."""
 
-    def __init__(self, segments, next_possible_keys):
+    def __init__(self, segments, next_possible_keys, or_ends_here=False):
         super(SuffixAmbiguous, self).__init__(segments)
         self.next_possible_keys = next_possible_keys
+        self.or_ends_here = or_ends_here
 
     def __str__(self):
-        return 'Ambiguous path: %s could continue as any of %s.' % (''.join(self.segments), self.next_possible_keys)
+        ends_here_msg = ' Or it could end without any of them.' if self.or_ends_here else ''
+        return 'Ambiguous path: %s could continue as any of %s.%s' % (''.join(self.segments), self.next_possible_keys, ends_here_msg)
--- tests/test_suffix_tree.py
+++ tests/test_suffix_tree.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import raises
 
 from sphinx_js.suffix_tree import SuffixAmbiguous, SuffixNotFound, SuffixTree
 
@@ -10,10 +10,10 @@ def test_things():
     s.add(['./', 'dir/', 'footils.', 'hacks'], 3)
 
     assert s.get(['hacks']) == 3
-    assert s.get(['footils.', 'max']) == 2
-    with pytest.raises(SuffixNotFound):
+    assert s.get_with_path(['footils.', 'max']) == (2, ['./', 'dir/', 'footils.', 'max'])
+    with raises(SuffixNotFound):
         s.get(['quacks.', 'max'])
-    with pytest.raises(SuffixAmbiguous):
+    with raises(SuffixAmbiguous):
         s.get(['max'])
 
 
@@ -21,4 +21,49 @@ def test_full_path():
     """Looking up a full path should not crash."""
     s = SuffixTree()
     s.add(['./', 'dir/', 'footils.', 'jacks'], 4)
-    assert s.get(['./', 'dir/', 'footils.', 'jacks']) == 4
+    assert s.get_with_path(['./', 'dir/', 'footils.', 'jacks']) == (4, ['./', 'dir/', 'footils.', 'jacks'])
+
+
+def test_terminal_insertion():
+    """A non-terminal segment should be able to later be made terminal."""
+    s = SuffixTree()
+    s.add(['a', 'b'], 5)
+    s.add(['b'], 4)
+    with raises(SuffixAmbiguous):
+        s.get('b')
+
+
+def test_ambiguous_even_if_full_path():
+    """Even full paths should be considered ambiguous if there are paths that
+    have them as suffixes."""
+    s = SuffixTree()
+    s.add(['a', 'b'], 5)
+    s.add(['q', 'a', 'b'], 6)
+    with raises(SuffixAmbiguous):
+        s.get(['a', 'b'])
+
+
+def test_ambiguous_paths_reported():
+    """Make sure SuffixAmbiguous gives a good explanation."""
+    s = SuffixTree()
+    s.add(['q', 'b', 'c'], 5)
+    s.add(['r', 'b', 'c'], 6)
+    try:
+        s.get(['b', 'c'])
+    except SuffixAmbiguous as exc:
+        assert exc.next_possible_keys == ['q', 'r']
+        assert not exc.or_ends_here
+
+
+def test_value_ambiguity():
+    """If we're at the point of following single-subtree links, make sure we
+    throw SuffixAmbiguous if we encounter a value and a subtree at a given
+    point (b in this case)."""
+    s = SuffixTree()
+    s.add(['a', 'b', 'c'], 5)
+    s.add(['b', 'c'], 6)
+    try:
+        assert s.get(['c'])
+    except SuffixAmbiguous as exc:
+        assert exc.next_possible_keys == ['b']
+        assert exc.or_ends_here
