557d5b29b3dcd474ffdbdc81fd03a4ae2a643080
lonnen <chris.lonnen@gmail.com>
2022-04-20 01:15:41 -0700

    pin specific library versions
    sphinx-js assumes semver, but both Jinja2 and Sphinx have removed functionality in minor version bumps that broke this library. This pins specific versions instead of relying on major version ranges.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(exclude=['ez_setup']),
     url='https://github.com/mozilla/sphinx-js',
     include_package_data=True,
-    install_requires=['Jinja2>3.0,<4.0', 'parsimonious>=0.9.0,<0.10.0', 'Sphinx>=4.0.0,<5.0.0'],
+    install_requires=['Jinja2==3.1.1', 'parsimonious==0.9.0', 'Sphinx==4.5.0'],
     python_requires='>=3.7',
     classifiers=[
         'Intended Audience :: Developers',
