a4cffe47b600305702ded8c3d62dd51b1faa2258
Will Kahn-Greene <willkg@mozilla.com>
2023-09-28 13:27:59 -0400

    Remove Mozilla-centric things (#233)

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(
     description='Support for using Sphinx on JSDoc-documented JS code',
     long_description=open('README.rst', 'r', encoding='utf8').read(),
     long_description_content_type="text/x-rst",
-    author='Erik Rose',
-    author_email='erikrose@grinchcentral.com',
     license='MIT',
     packages=find_packages(exclude=['ez_setup']),
     url='https://github.com/mozilla/sphinx-js',
