8d99ead4dabd78af7582879d157d0a0863155336
Will Kahn-Greene <willkg@mozilla.com>
2024-12-23 18:48:18 -0500

    Add support for Python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         'Topic :: Documentation :: Sphinx',
         'Topic :: Software Development :: Documentation'
     ],
